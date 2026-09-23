1e884f02abecd024b185b2fa0636ef4dc95cd57b
Rioux Baptiste <brioux@iutbayonne.univ-pau.fr>
2026-09-23 16:20:31 +0200

    feat: ajout de la méthode ensembleChiffres et de ses tests unitaires

--- src/main/java/com/baptiste/calculator/Calculator.java
+++ src/main/java/com/baptiste/calculator/Calculator.java
@@ -1,5 +1,8 @@
 package com.baptiste.calculator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calculator {
     public static int add(int opG, int opD) {
         return opG + opD;
@@ -7,5 +10,14 @@ public class Calculator {
 
     public static int divide(int opG, int opD) {
         return opG / opD;
+    }
+
+    public Set<Integer> ensembleChiffres(int pNombre) {
+        Set<Integer> chiffres = new HashSet<>();
+        String str = String.valueOf(Math.abs((long) pNombre));
+        for (char c : str.toCharArray()) {
+            chiffres.add(c - '0');
+        }
+        return chiffres;
     }
 }
--- src/test/java/com/baptiste/calculator/CalculatorTest.java
+++ src/test/java/com/baptiste/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package com.baptiste.calculator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
@@ -31,5 +32,22 @@ public class CalculatorTest {
         assertThat(Calculator.divide(6, 2)).isEqualTo(3);
         assertThat(Calculator.divide(7, 2)).isEqualTo(3);
         assertThat(Calculator.divide(-6, 2)).isEqualTo(-3);
+    }
+
+    @Test
+    void testEnsembleChiffres() {
+        Calculator calculator = new Calculator();
+
+        Set<Integer> resultatExemple1 = calculator.ensembleChiffres(7679);
+        assertThat(resultatExemple1).containsExactlyInAnyOrder(6, 7, 9);
+
+        Set<Integer> resultatExemple2 = calculator.ensembleChiffres(-11);
+        assertThat(resultatExemple2).containsExactly(1);
+
+        Set<Integer> resultatZero = calculator.ensembleChiffres(0);
+        assertThat(resultatZero).containsExactly(0);
+
+        Set<Integer> resultatAutre = calculator.ensembleChiffres(-505);
+        assertThat(resultatAutre).containsExactlyInAnyOrder(0, 5);
     }
 }
