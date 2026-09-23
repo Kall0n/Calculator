fe58cc5cde6868d8fc44cd5edd7682b88a3bdf09
Rioux Baptiste <brioux@iutbayonne.univ-pau.fr>
2026-09-23 16:11:50 +0200

    refactor: passage des méthodes add et divide en statique et mise à jour des tests

--- src/test/java/com/baptiste/calculator/CalculatorTest.java
+++ src/test/java/com/baptiste/calculator/CalculatorTest.java
@@ -1,35 +1,21 @@
 package com.baptiste.calculator;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
-    private Calculator calculator;
-
-    @BeforeEach
-    void setUp() {
-        calculator = new Calculator();
-    }
-
-    @AfterEach
-    void tearDown() {
-        calculator = null;
-    }
-
     @Test
     void testAdd() {
-        assertThat(calculator.add(2, 3)).isEqualTo(5);
-        assertThat(calculator.add(-2, 3)).isEqualTo(1);
-        assertThat(calculator.add(0, 0)).isEqualTo(0);
+        assertThat(Calculator.add(2, 3)).isEqualTo(5);
+        assertThat(Calculator.add(-2, 3)).isEqualTo(1);
+        assertThat(Calculator.add(0, 0)).isEqualTo(0);
     }
 
     @Test
     void testDivide() {
-        assertThat(calculator.divide(6, 2)).isEqualTo(3);
-        assertThat(calculator.divide(7, 2)).isEqualTo(3);
-        assertThat(calculator.divide(-6, 2)).isEqualTo(-3);
+        assertThat(Calculator.divide(6, 2)).isEqualTo(3);
+        assertThat(Calculator.divide(7, 2)).isEqualTo(3);
+        assertThat(Calculator.divide(-6, 2)).isEqualTo(-3);
     }
 }
