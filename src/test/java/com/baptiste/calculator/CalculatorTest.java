d11a57c28168fc2cec7ada700e78844cf3e9ee61
Rioux Baptiste <brioux@iutbayonne.univ-pau.fr>
2026-09-23 16:09:11 +0200

    refactor: utilisation de @BeforeEach et @AfterEach pour simplifier les tests

--- src/test/java/com/baptiste/calculator/CalculatorTest.java
+++ src/test/java/com/baptiste/calculator/CalculatorTest.java
@@ -1,13 +1,26 @@
 package com.baptiste.calculator;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculator = null;
+    }
+
     @Test
     void testAdd() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.add(2, 3)).isEqualTo(5);
         assertThat(calculator.add(-2, 3)).isEqualTo(1);
         assertThat(calculator.add(0, 0)).isEqualTo(0);
@@ -15,7 +28,6 @@ public class CalculatorTest {
 
     @Test
     void testDivide() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.divide(6, 2)).isEqualTo(3);
         assertThat(calculator.divide(7, 2)).isEqualTo(3);
         assertThat(calculator.divide(-6, 2)).isEqualTo(-3);
